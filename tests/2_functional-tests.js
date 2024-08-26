95126c1ec4dbd8894d69968e2b2669c4be5120c6
hardi-yansah <hardi.yansah97@gmail.com>
2024-08-26 15:03:36 +0700

    fix: update ConvertHandler.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -30,7 +30,7 @@ suite('Functional Tests', function () {
                     .query({ input: '32g' })
                     .end(function (err, res) {
                         assert.equal(res.status, 200);
-                        assert.equal(res.body.error, undefined);
+                        assert.equal(res.body.error, 'invalid unit');
                         done();
                     });
             });
@@ -41,7 +41,7 @@ suite('Functional Tests', function () {
                     .query({ input: '3/7.2/4kg' })
                     .end(function (err, res) {
                         assert.equal(res.status, 200);
-                        assert.equal(res.body.error, undefined);
+                        assert.equal(res.body.error, 'invalid unit');
                         done();
                     });
             });
@@ -52,7 +52,7 @@ suite('Functional Tests', function () {
                     .query({ input: '3/7.2/4kilomegagram' })
                     .end(function (err, res) {
                         assert.equal(res.status, 200);
-                        assert.equal(res.body.error, undefined);
+                        assert.equal(res.body.error, 'invalid unit');
                         done();
                     });
             });
